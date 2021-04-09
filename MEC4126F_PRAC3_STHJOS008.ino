b4479ccdd9bf528badbb228773c7337314a80868
DESKTOP-VA6PKMF\User <STHJOS008@myuct.ac.za>
2021-04-09 04:39:04 +0200

    Add: Prac3, q2.2 code
    1) Removing code from prac 2 that is not required in prac 3.
    2) Declaring a struct of type "myData"

--- MEC4126F_PRAC3_STHJOS008.ino
+++ MEC4126F_PRAC3_STHJOS008.ino
@@ -1,44 +1,16 @@
-void displayOnLEDs(uint8_t number);
-
-struct AgeData
+struct myData		// declare a struct of type "myData"
 {
-  uint8_t BirthDateOfMonth;
-  uint8_t BirthMonth;
-  uint16_t BirthYear;
   uint8_t Age;
-}
-myAge;
+  char FirstName[7];
+} person;			// create a variable of "myData" called "person"
+
 
 void setup()
 {
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  
-  myAge.BirthDateOfMonth = 31;
-  myAge.BirthMonth = 5;
-  myAge.BirthYear = 1997;
-  myAge.Age = 23;
+
 }
 
 void loop()
 {
-  for (uint8_t i = 0; i <= myAge.Age; i++)
-  {
-    displayOnLEDs(i);
-    delay(100);
-  }
-}
 
-void displayOnLEDs(uint8_t number);
-{
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    digitalWrite(i, (number & (0b1 << i)) ? HIGH : LOW);
-  }
 }
